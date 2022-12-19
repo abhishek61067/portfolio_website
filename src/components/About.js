00a5aec1438f60ada4ce5162057264a7a38d2800
abhishek <abhishek.bhattarai61067@gmail.com>
2022-12-19 20:16:53 +0545

    removed about me

--- src/components/About.js
+++ src/components/About.js
@@ -1,25 +1,6 @@
 import React from "react";
-import Aboutimg from "../about.png"
+import Aboutimg from "../about.png";
 
-const About = () => {
-  return (
-  <div className="about-section bg-light">
-<div className="container mt-5 ">
-    <div className="row title text-center mb-5">
-        <h2>about me</h2>
-    </div>
-    <div className="row">
-        <div className="col-md-6">
-            <img src={Aboutimg} alt="" width={"100%"} />
-        </div>
-        <div className="col-md-6">
-<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laudantium sapiente numquam obcaecati id nesciunt. Quis, omnis officia quas in alias nihil suscipit quae ipsa quod maxime ipsum blanditiis, dolorum dolor.</p>
-        </div>
-    </div>
-</div>
-  </div>
-  );
-};
+const About = () => {};
 
 export default About;
-
